75c04dccb8bda3368512ac2bdf6ac9887e188596
Arsen <106221641+projacktor@users.noreply.github.com>
2024-06-30 19:52:51 +0300

    Add TypeScript

---
+++ assignment3/vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    build: {
+        outDir: 'dist',
+        rollupOptions: {
+            output: {
+                entryFileNames: 'assets/[name].[hash].js',
+                chunkFileNames: 'assets/[name].[hash].js',
+                assetFileNames: 'assets/[name].[hash].[ext]'
+            }
+        }
+    }
+});
