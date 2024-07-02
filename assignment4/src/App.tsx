63e47b6be699dae436184f576843ad24ad350c88
Arsen <106221641+projacktor@users.noreply.github.com>
2024-07-02 12:20:39 +0300

    Add TypingGreeting widget

--- assignment4/src/App.tsx
+++ assignment4/src/App.tsx
@@ -1,9 +1,17 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+
 import Header from "./components/widgets/Header/Header.tsx";
+import Home from "./components/pages/Home/Home.tsx";
 
 function App() {
   return (
     <div className="App">
       <Header/>
+        <BrowserRouter basename="/FWD_sum24/">
+            <Routes>
+                <Route path="" element={<Home/>}/>
+            </Routes>
+        </BrowserRouter>
     </div>
   )
 }
---
+++ assignment4/src/components/widgets/Typings/Tiyping Greeting/TypingGreeting.tsx
@@ -0,0 +1,25 @@
+import "./style.css";
+import "../typing_style.css";
+
+import {useEffect} from "react";
+import {initTypingGreeting} from "../../../../functions/typing.ts";
+
+import li7Ico from "../../../../assets/svg/Li7ico.svg";
+import iuIco from "../../../../assets/svg/IU_icon.svg";
+
+function TypingGreeting() {
+    useEffect(() => {
+        initTypingGreeting();
+    }, []);
+    return (
+        <div className="typing_container white container greeting_cont">
+            <div id="typing_img">
+                <img alt="Lyceum picture" src={li7Ico as string} id="li7" className="hidden"/>
+                <img alt="IU picture" src={iuIco as string} id="iu" className="hidden"/>
+            </div>
+            <div id="typing_text_greeting" className="typing"></div>
+        </div>
+    );
+}
+
+export default TypingGreeting;
