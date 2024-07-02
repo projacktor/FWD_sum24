053a2c75b8e239229afc534dcd4b3f92ec362ead
Arsen <106221641+projacktor@users.noreply.github.com>
2024-07-02 14:21:53 +0300

    Changes for deploy

--- assignment4/vite.config.ts
+++ assignment4/vite.config.ts
@@ -1,18 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import {resolve} from "path";
+// import {resolve} from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/FWD_sum24/",
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: {
-        index: resolve(__dirname, "index.html"),
-        comic: resolve(__dirname, "comic.html"),
-      }
-    }
-  }
+  // build: {
+  //   outDir: 'dist',
+  //   rollupOptions: {
+  //     input: {
+  //       index: resolve(__dirname, "index.html"),
+  //       comic: resolve(__dirname, "404.html"),
+  //     }
+  //   }
+  // }
 })
