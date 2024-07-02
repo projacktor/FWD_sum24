cfe463ab41113b7f96188b7ac4bf6973a420cfee
Arsen <106221641+projacktor@users.noreply.github.com>
2024-07-02 14:04:24 +0300

    Add 404 page

--- assignment4/src/App.tsx
+++ assignment4/src/App.tsx
@@ -3,6 +3,7 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Header from "./components/widgets/Header/Header.tsx";
 import Home from "./components/pages/Home/Home.tsx";
 import Comic from "./components/pages/Comic/Comic.tsx";
+import PageNotFound from "./components/pages/404/PageNotFound.tsx";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
             <Routes>
                 <Route path="" element={<Home/>}/>
                 <Route path="/comic" element={<Comic/>}/>
+                <Route path="*" element={<PageNotFound/>}/>
             </Routes>
         </BrowserRouter>
     </div>
