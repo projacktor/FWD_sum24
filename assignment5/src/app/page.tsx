48dc2710ca52b1b544fcbc843554c23f8ff95eb5
Arsen <106221641+projacktor@users.noreply.github.com>
2024-07-05 16:07:50 +0300

    Fix typing

--- assignment5/src/app/page.tsx
+++ assignment5/src/app/page.tsx
@@ -6,18 +6,19 @@ import TypingStack from "@/components/widgets/Typings/Typing Stack/TypingStack";
 import ProjectsGallery from "@/components/widgets/Projects gallery/ProjectsGallery";
 
 export default function Home() {
-  return (
-      <main className={styles.home}>
-        <div className={styles.headers}>
-          <h1 className={styles.h1}>Galiev <span className="pink">Arsen</span>'s</h1>
-          <h2 className={styles.h2}><span className="pink">private</span> pages</h2>
-        </div>
+    return (
+        <main className={styles.home}>
+            <div className={styles.headers}>
+                <h1 className={styles.h1}>Galiev <span className="pink">Arsen</span>'s</h1>
+                <h2 className={styles.h2}><span className="pink">private</span> pages</h2>
+            </div>
+            <div className={styles.type}>
+                <TypingGreeting/>
 
-          <TypingGreeting/>
+                <TypingStack/>
+            </div>
 
-          <TypingStack/>
-
-          <ProjectsGallery/>
-      </main>
-  );
+            <ProjectsGallery/>
+        </main>
+    );
 }
