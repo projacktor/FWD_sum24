ff66e509b692f84102aa6c7181704845a0ad6142
Arsen <106221641+projacktor@users.noreply.github.com>
2024-07-06 03:30:11 +0300

    Fix next.config & package.json for deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: '/FWD_sum24',
+  output: 'export',
+  reactStrictMode: true,
   images: {
     domains: ['imgs.xkcd.com'],
   },
