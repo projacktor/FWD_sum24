11ae279caf960a6f797d6bdc02e382c6f553c97b
Arsen <106221641+projacktor@users.noreply.github.com>
2024-07-11 10:52:53 +0300

    deploy: prepaer project for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/FWD_sum24',
+  // basePath: '/FWD_sum24',
   // output: 'export',
   reactStrictMode: true,
   images: {
